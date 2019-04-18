f5df8563edb8ef91315651987deac98042abc6f6
Benjamin Wang <benjamin@Benjaminde-MacBook-Pro-2.local>
2019-04-19 02:03:33 +0800

    :tada: Upload Slides

--- README.md
+++ README.md
@@ -1,3 +1,62 @@
-# TECH-K-NOW-Day
-2019.4.20  
+# TECH(K)NOW DAY TAIPEI
+Saturday, 20 April 2019 from 09:00 to 18:00 (CST)  
 Workshop 03 - GoLang  
+  
+The repository is the example code of the Golang workshop
+  
+Links:  
+[Event Details and Ticket Information](https://www.eventbrite.co.uk/e/techknow-day-taipei-saturday-april-20-9am-to-6pm-tickets-55343822864)  
+  
+## Let's Go for Web!
+How to build a web service with golang step-by-step.  
+## Slide Contents
+1. Intrudoction  
+2. Install  
+3. Basic Knowledge  
+4. RESTful API  
+5. HTML Template  
+6. Static Assets  
+7. Post Form  
+8. User Login  
+9. Cookie  
+10. Socket  
+
+## Installation
+  
+To run the example, you need to install gin and melody
+```sh
+$ go get -u github.com/gin-gonic/gin
+$ go get -u gopkg.in/olahol/melody.v1
+```
+  
+## Quick Start
+```sh
+# run main.go and visit localhost:8080/ on browser
+$ go run main.go
+```
+### Web routers
+
+localhost:8080/  
+localhost:8080/login  
+localhost:8080/chat  
+localhost:8080/ws
+
+### File tree
+```
+.
+├── README.md
+├── Slides
+│   ├── 01. Introduction.pptx
+│   ├── 02. RESTful API Service.pptx
+│   └── 03. User Login.pptx
+├── assets
+│   └── images
+│       └── gopher.png
+├── main.go
+└── templates
+    ├── chat.html.tmpl
+    ├── header.html.tmpl
+    ├── index.html.tmpl
+    └── login.html.tmpl
+```
+
--- main.go
+++ main.go
@@ -1,16 +1,21 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	melody "gopkg.in/olahol/melody.v1"
 )
+
+var title = "Hello TECH(K)NOW Day"
 
 func main() {
 	log.Println("Start web service")
 
 	r := gin.Default()
+	m := melody.New()
 
 	r.Static("/assets", "./assets")
 
@@ -18,19 +23,50 @@ func main() {
 
 	r.GET("/", index)
 
+	r.GET("/login", loginPage)
+
 	r.POST("/login", login)
+
+	r.GET("/chat", chat)
+
+	r.GET("/ws", func(c *gin.Context) {
+		m.HandleRequest(c.Writer, c.Request)
+	})
+
+	m.HandleMessage(func(s *melody.Session, msg []byte) {
+		m.Broadcast(msg)
+	})
 
 	r.Run(":8080")
 }
 
 func index(c *gin.Context) {
 	c.HTML(http.StatusOK, "index.html.tmpl", gin.H{
-		"Title": "Hello TECH(K)NOW Day",
+		"Title": title,
 	})
 }
 
 func login(c *gin.Context) {
 	username := c.PostForm("username")
+	password := c.PostForm("password")
 	log.Println("username:", username)
-	c.String(http.StatusOK, "Hi "+username)
+	if username == "Benjamin" && password == "1234" {
+		c.SetCookie("username", username, 60, "/", "localhost", false, true)
+		c.String(http.StatusOK, "Hi "+username)
+	} else {
+		c.String(http.StatusOK, fmt.Sprintf("%s password error", username))
+	}
+}
+
+func loginPage(c *gin.Context) {
+	c.HTML(http.StatusOK, "login.html.tmpl", gin.H{
+		"Title": title,
+	})
+}
+
+func chat(c *gin.Context) {
+	username, _ := c.Cookie("username")
+	c.HTML(http.StatusOK, "chat.html.tmpl", gin.H{
+		"username": username,
+	})
 }
